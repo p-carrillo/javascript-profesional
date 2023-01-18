507d3252b0baf3ec57ee9415a4c703b1ab76caf3
p-carrillo <pablo.carrillo.garcia@gmail.com>
2023-01-18 08:09:13 +0100

    Class 5 Plugins

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,13 @@
   import MediaPlayer from './mediaplayer.js'
+  import Autoplay from './plugins/autoplay.js'
 
   const video = document.querySelector('video');
-  const player = new MediaPlayer({ el: video });
+  const player = new MediaPlayer({ el: video , plugins: [
+    new Autoplay(),
+  ]});
 
-  const button = document.querySelector('button');
-  button.onclick = () => player.togglePlay();
+  const playButton = document.getElementById('play_toogle');
+  const muteButton = document.getElementById('mute_toogle');
+
+  playButton.onclick = () => player.togglePlay();
+  muteButton.onclick = () => player.toggleSound();
